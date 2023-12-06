144bcdc7bd4eb668c47cc6e213f46696975c3c7b
Dmitry Mormul <macairmormul@MacBook-Air-Dmitry.local>
2023-12-06 20:17:44 +0100

    Created purple design

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
-import video from "../../assets/home/video.mp4";
-import { observer, observer_op } from "../../observers.js";
+import video from "../../assets/home/video2.mp4";
+import { observer_op } from "../../observers.js";
 import "./Video.scss";
 
 const words = [
@@ -13,35 +13,53 @@ const words = [
 ];
 
 const colors = [
-  "#E58022",
-  "#CF193F",
-  "#1064E0",
-  "#E58022",
-  "#CF193F",
-  "#1064E0",
+  "#A975FE",
+  "#FE3BD6",
+  "#FFDD2C",
+  "#A975FE",
+  "#FE3BD6",
+  "#FFDD2C",
 ];
 
 export const Video = () => {
-  const p = useRef(null)
+  const p = useRef(null);
+  const videoRef = useRef(null);
+  const [again, setAgain] = useState(false);
   const [word, setWord] = useState(words[0]);
   const [color, setColor] = useState(colors[0]);
   const videoCont = useRef(null);
 
   useEffect(() => {
-    if (p.current) {
-      p.current.style.transition = 'all .7s'
-      observer_op().observe(p.current)
+    if (videoRef.current) {
+      videoRef.current.style.transition = "all .7s";
+      observer_op().observe(videoRef.current);
     }
-  },[])
+    if (p.current) {
+      p.current.style.transition = "all .7s";
+      observer_op().observe(p.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    words.forEach((el, i) =>
+      setTimeout(() => {
+        setWord(el);
+        setColor(colors[i]);
+        if (i === words.length - 1) setAgain((prev) => !prev);
+      }, 1000 * (i + 1))
+    );
+  }, [again]);
 
   return (
     <div className="video-outer" ref={videoCont}>
-      <h1>Let Expansion 4 YOU <br /> Make It Happen!</h1>
+      <h1>
+        Let Expansion 4 YOU <br /> Make It Happen!
+      </h1>
       <hr />
-      <div className="video-inner">
+      <div className="video-inner" ref={videoRef}>
         <video
           src={video}
-          // autoPlay={true}
+          autoPlay={true}
           muted={true}
           loop={true}
           type="video/mp4"
